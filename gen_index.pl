ae316e6e1a2e08b02a004dd1a23e89284dc79b06
Kyle Milz <kyle@getaddrinfo.net>
2014-11-05 22:23:00 -0700

    gen_index: use result directly

--- gen_index.pl
+++ gen_index.pl
@@ -20,18 +20,18 @@ my $config = {
 my $template = Template->new($config);
 
 my $query = "select count(distinct manufacturer) from products";
-my @manuf = $dbh->selectrow_array($query);
+my ($manufacturers) = $dbh->selectrow_array($query);
 
 $query = "select count(part_num) from products";
-my @products = $dbh->selectrow_array($query);
+my ($products) = $dbh->selectrow_array($query);
 
 $query = "select count(name) from vendors";
-my @vendors = $dbh->selectrow_array($query);
+my ($vendors) = $dbh->selectrow_array($query);
 
 my $vars = {
-	num_vendors => $vendors[0],
-	num_manufacturers => $manuf[0],
-	num_products => $products[0]
+	num_vendors => $vendors,
+	num_manufacturers => $manufacturers,
+	num_products => $products
 };
 
 $template->process("index.html", $vars, "index.html") || die $template->error();
