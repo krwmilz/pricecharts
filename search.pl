ed3383edce48886b507a163ec6fd290e8d3f1284
Kyle Milz <kyle@getaddrinfo.net>
2014-11-09 19:46:57 -0700

    search: add timestamps to log

--- search.pl
+++ search.pl
@@ -6,6 +6,7 @@ use warnings;
 use FCGI;
 use Template;
 use Proc::Daemon;
+use POSIX;
 
 use shared;
 
@@ -31,25 +32,26 @@ $SIG{TERM} = \&sig_handler;
 
 my $socket_path = "/var/www/run/search.sock";
 my $socket = FCGI::OpenSocket($socket_path, 1024);
+print ftime() . "socket created on $socket_path\n";
 my $request = FCGI::Request(\*STDIN, \*STDOUT, \*STDERR, \%ENV,
 	$socket, FCGI::FAIL_ACCEPT_ON_INTR);
-print "made socket and request objects\n";
+print ftime() . "fcgi request object created\n";
 
 my $config = {
 	# XXX: this needs to be fixed
 	INCLUDE_PATH => "/home/kyle/src/pricechart/html"
 };
 my $template = Template->new($config);
-print "made new template config\n";
+print ftime() . "templete config created\n";
 
 my $dbh = get_dbh();
-print "opened db\n";
+print ftime() . "database opened\n";
 
 my $sql = "select part_num, manufacturer, description from products " .
 	"where description like ? or part_num like ? or manufacturer like ?";
 my $search_sth = $dbh->prepare($sql);
 
-print "starting main loop\n";
+print ftime() . "starting main accept loop\n";
 while ($request->Accept() >= 0) {
 	print "Content-Type: text/html\r\n\r\n";
 
@@ -74,7 +76,7 @@ while ($request->Accept() >= 0) {
 	}
 }
 
-print "shutting down\n";
+print ftime() . "shutting down\n";
 FCGI::CloseSocket($socket);
 unlink($socket_path, $pid_file);
 $dbh->disconnect();
@@ -82,5 +84,10 @@ $dbh->disconnect();
 sub sig_handler
 {
 	$request->LastCall();
-	print "caught signal\n";
+	print ftime() . "caught signal\n";
+}
+
+sub ftime
+{
+	return strftime "%b %e %Y %H:%M ", localtime;
 }
