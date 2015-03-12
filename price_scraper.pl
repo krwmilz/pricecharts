d5ed3e7bce244d94b4b3943585c4cf2d68057d12
Kyle Milz <kyle@getaddrinfo.net>
2015-03-12 01:34:47 -0600

    price_scraper: send verbosity to get_dbh

--- price_scraper.pl
+++ price_scraper.pl
@@ -22,7 +22,7 @@ $| = 1 if ($args{v});
 my $log = get_log("scrapes", $args{v});
 my $cfg = get_config();
 my $ua  = new_ua($cfg->{"general"}, $args{v});
-my $dbh = get_dbh($cfg->{"general"});
+my $dbh = get_dbh($cfg->{"general"}, undef, $args{v});
 
 # allow products to go out of stock. if we haven't seen them for > 30 days
 # chances are retailers aren't carrying them anymore
