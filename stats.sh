5824576fe911689dfcba9d154926533d21276772
kyle <kyle@getaddrinfo.net>
2015-11-02 19:45:55 -0700

    stats.sh: add mandatory database argument

--- stats.sh
+++ stats.sh
@@ -1,8 +1,13 @@
 #!/bin/ksh
 
-sql_cmd="sqlite3 /var/db/pricesloth/db"
+if [ $# -ne 1 ]; then
+	echo "usage: ${0} <db>"
+	exit 1
+fi
 
-printf "prices:       %i\n" $($sql_cmd "select count(*) from prices;")
+sql_cmd="sqlite3 ${1}"
+
 printf "products:     %i\n" $($sql_cmd "select count(*) from products;")
+printf "prices:       %i\n" $($sql_cmd "select count(*) from prices;")
 printf "descriptions: %i\n" $($sql_cmd "select count(*) from descriptions;")
 printf "retailers:    %i\n" $($sql_cmd "select count(*) from retailers;")
